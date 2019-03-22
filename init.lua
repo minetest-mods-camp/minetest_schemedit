8f7dbdc56f07240ef6cda4bc3f9fc4509c7561b3
Wuzzy <Wuzzy2@mail.ru>
2019-03-22 18:39:44 +0100

    Fix a few typos

--- init.lua
+++ init.lua
@@ -827,9 +827,9 @@ minetest.register_node("schemedit:creator", {
 	description = S("Schematic Creator"),
 	_doc_items_longdesc = S("The schematic creator is used to save a region of the world into a schematic file (.mts)."),
 	_doc_items_usagehelp = S("To get started, place the block facing directly in front of any bottom left corner of the structure you want to save. This block can only be accessed by the placer or by anyone with the “schematic_override” privilege.").."\n"..
-S("To save a region, rightclick the block, enter the size, a schematic name and hit “Export schematic”. The file will always be saved in the world directory. Note you can use this name in the /placeschem command to place the schematic again.").."\n\n"..
+S("To save a region, use the block, enter the size and a schematic name and hit “Export schematic”. The file will always be saved in the world directory. Note you can use this name in the /placeschem command to place the schematic again.").."\n\n"..
 S("The other features of the schematic creator are optional and are used to allow to add randomness and fine-tuning.").."\n\n"..
-S("Y slices are used to remove entire slices based on chance. For each slice of the schematic region along the Y axis, you can specify that it occours only with a certain chance. In the Y slice tab, you have to specify the Y slice height (0 = bottom) and a probability from 0 to 255 (255 is for 100%). By default, all Y slices occour always.").."\n\n"..
+S("Y slices are used to remove entire slices based on chance. For each slice of the schematic region along the Y axis, you can specify that it occurs only with a certain chance. In the Y slice tab, you have to specify the Y slice height (0 = bottom) and a probability from 0 to 255 (255 is for 100%). By default, all Y slices occur always.").."\n\n"..
 S("With a schematic node probability tool, you can set a probability for each node and enable them to overwrite all nodes when placed as schematic. This tool must be used prior to the file export."),
 	tiles = {"schemedit_creator_top.png", "schemedit_creator_bottom.png",
 			"schemedit_creator_sides.png"},
@@ -891,7 +891,7 @@ minetest.register_tool("schemedit:probtool", {
 S("This is an advanced tool which only makes sense when used together with a schematic creator. It is used to finetune the way how nodes from a schematic are placed.").."\n"..
 S("It allows you to set two things:").."\n"..
 S("1) Set probability: Chance for any particular node to be actually placed (default: always placed)").."\n"..
-S("2) Enable force placement: These nodes replace node other than air and ignored when placed in a schematic (default: off)"),
+S("2) Enable force placement: These nodes replace node other than air and ignore when placed in a schematic (default: off)"),
 	_doc_items_usagehelp = "\n"..
 S("BASIC USAGE:").."\n"..
 S("Punch to configure the tool. Select a probability (0-255; 255 is for 100%) and enable or disable force placement. Now place the tool on any node to apply these values to the node. This information is preserved in the node until it is destroyed or changed by the tool again. This tool has no effect on schematic voids.").."\n"..
@@ -900,7 +900,7 @@ S("NODE HUD:").."\n"..
 S("To help you remember the node values, the nodes with special values are labelled in the HUD. The first line shows probability and force placement (with “[F]”). The second line is the current distance to the node. Nodes with default settings and schematic voids are not labelled.").."\n"..
 S("To disable the node HUD, unselect the tool or hit “place” while not pointing anything.").."\n\n"..
 S("UPDATING THE NODE HUD:").."\n"..
-S("The node HUD is not updated automatically any may be outdated. The node HUD only updates the HUD for nodes close to you whenever you place the tool or press the punch and sneak keys simutanously. If you sneak-punch a schematic creator, then the node HUd is updated for all nodes within the schematic creator's region, even if this region is very big."),
+S("The node HUD is not updated automatically and may be outdated. The node HUD only updates the HUD for nodes close to you whenever you place the tool or press the punch and sneak keys simutanously. If you sneak-punch a schematic creator, then the node HUD is updated for all nodes within the schematic creator's region, even if this region is very big."),
 	wield_image = "schemedit_probtool.png",
 	inventory_image = "schemedit_probtool.png",
 	liquids_pointable = true,
